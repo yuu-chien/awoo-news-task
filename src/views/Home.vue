e743ddd95e42bebbd197adba4e6fc18b75b1b459
yuu-chien <55533267+yuu-chien@users.noreply.github.com>
2021-09-10 12:34:54 +0800

    Home : Add newsCards list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="row">
+    <h2 class="col-md-12 fw-bold fs-3">Find New Stuff</h2>
+    <div class="col-md-12 mb-5">
+      <div class="w-100">
+        <!-- <label for="" class="form-label"></label> -->
+        <input type="text" class="form-control" id="" placeholder="COVID-19" />
+      </div>
+      <button type="button" class="btn btn-outline-info">Search</button>
+    </div>
+    <div class="col-md-12 col-lg-6" v-for="(item, i) in allnews" :key="i">
+      <NewsCard :news="item"></NewsCard>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import NewsCard from "@/components/NewsCard.vue";
 export default {
-  name: "Home",
   components: {
-    HelloWorld,
+    NewsCard,
+  },
+  props: ["news"],
+  data() {
+    return {
+      uuid: process.env.VUE_APP_UUID,
+      allnews: [],
+    };
+  },
+  methods: {
+    getNews() {
+      this.axios
+        .get(`https://newsapi.org/v2/everything?q=bitcoin&apiKey=${this.uuid}`)
+        .then((res) => {
+          if (res.status === 200) {
+            this.allnews = res.data.articles;
+          }
+        });
+    },
+  },
+  created() {
+    this.getNews();
   },
 };
 </script>
