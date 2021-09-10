6949c265eef5bbf4313c9a7eacae3ac3c01c44d4
yuu-chien <55533267+yuu-chien@users.noreply.github.com>
2021-09-10 12:33:06 +0800

    Component : Add NewsCard

---
+++ src/components/NewsCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="c-newsCard border rounded shadow-sm mb-4">
+    <div class="c-newsCard__cnt col-7 p-3">
+      <div class="">
+        <div class="c-newsCard__tit text-info mb-2">{{ news.title }}</div>
+        <div class="c-newsCard__subtit text-muted">
+          {{ news.publishedAt }} by {{ news.author }}
+        </div>
+      </div>
+      <p class="c-newsCard__dec">{{ news.description }}</p>
+      <a href="#" class="c-newsCard__link text-primary text-opacity-50"
+        >Continue reading</a
+      >
+    </div>
+    <div class="c-newsCard__img col-5">
+      <img :src="news.urlToImage" :alt="news.title" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["news"],
+};
+</script>
