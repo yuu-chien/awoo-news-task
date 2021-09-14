27316cab7f0be619e872fe06259827217677ef1e
yuu-chien <55533267+yuu-chien@users.noreply.github.com>
2021-09-14 23:22:34 +0800

    Home : Add search and sortby

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,35 @@
   <div class="row">
     <h2 class="col-md-12 fw-bold fs-3">Find New Stuff</h2>
     <div class="col-md-12 mb-5">
-      <div class="w-100 mb-2">
-        <!-- <label for="" class="form-label"></label> -->
-        <input type="text" class="form-control" id="" placeholder="COVID-19" />
+      <div class="row">
+        <div class="col-md-5">
+          <input
+            type="text"
+            class="form-control"
+            id=""
+            placeholder="Keyword"
+            v-model="keyword"
+          />
+        </div>
+        <div class="col-md-5">
+          <select class="form-select" v-model="optionval">
+            <option disabled value="">Sort By...</option>
+            <option value="Popularity">Popularity</option>
+            <option value="Relevancy">Relevancy</option>
+          </select>
+        </div>
+        <div class="col-md-2">
+          <button
+            type="button"
+            class="btn btn-outline-info"
+            @click.prevent="getNews()"
+          >
+            Search
+          </button>
+        </div>
       </div>
-      <button type="button" class="btn btn-outline-info">Search</button>
     </div>
+
     <div class="col-md-12 col-lg-6" v-for="(item, i) in allnews" :key="i">
       <NewsCard :news="item"></NewsCard>
     </div>
@@ -25,12 +48,16 @@ export default {
     return {
       uuid: process.env.VUE_APP_UUID,
       allnews: [],
+      optionval: "",
+      keyword: "Taiwan",
     };
   },
   methods: {
     getNews() {
       this.axios
-        .get(`https://newsapi.org/v2/everything?q=bitcoin&apiKey=${this.uuid}`)
+        .get(
+          `https://newsapi.org/v2/everything?q=${this.keyword}&sortBy=${this.optionval}&apiKey=${this.uuid}`
+        )
         .then((res) => {
           if (res.status === 200) {
             this.allnews = res.data.articles;
